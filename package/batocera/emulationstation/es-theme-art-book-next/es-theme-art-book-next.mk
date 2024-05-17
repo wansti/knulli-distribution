841ba07519eea81f1699e67a58a0598142ca22a7
anthonycaccese <anthonycaccese@gmail.com>
2024-05-17 18:13:14 -0400

    Update es-theme-art-book-next.mk

--- package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Art Book Next"
 #
 ################################################################################
-# Version.: Commits on May 10, 2024
-ES_THEME_ART_BOOK_NEXT_VERSION = 2e775231c341ef6a7859beb052a3835cf64e3870
+# Version.: Commits on May 17, 2024
+ES_THEME_ART_BOOK_NEXT_VERSION = 76ab63e24cb1e2258d5e7294b6aa21b4e8f8e98e
 ES_THEME_ART_BOOK_NEXT_SITE = $(call github,anthonycaccese,art-book-next-es,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
