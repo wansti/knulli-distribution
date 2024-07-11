1d64cd4eb5a210a0f3eeff7d53caec20cae2e5d3
Marek Moeckel <marek@discarded-ideas.org>
2024-07-11 11:41:18 +1000

    Improve power button suspend behaviour

--- board/batocera/allwinner/h700/fsoverlay/usr/bin/lid_suspend.sh
+++ board/batocera/allwinner/h700/fsoverlay/usr/bin/lid_suspend.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-HALLKEY=/sys/class/power_supply/axp2202-battery/hallkey
+HALLKEY="/sys/class/power_supply/axp2202-battery/hallkey"
+POWER_BUTTON_STATE="/var/run/power-button-state.flag"
 
 while true; do
 	if [ "$(cat $HALLKEY)" = "0" ]; then
 		pm-is-supported --suspend && pm-suspend
+		# reset power button state on wakeup
+		echo "0" > "$POWER_BUTTON_STATE"
 	fi
 	sleep 1
 done
