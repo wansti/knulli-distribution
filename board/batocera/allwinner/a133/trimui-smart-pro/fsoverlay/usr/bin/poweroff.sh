858cd257a8df8cefcc6048b70b7c6a0fce5681f2
acmeplus <ctalon@gmail.com>
2024-07-12 08:30:59 -0400

    20240712
    * Reorganized TSP overlay to make it consistent with the recent h700
      changes
    * Added back the USB wifi drivers for rg28xx and RG35xx-2024
    * Fixed HDMI switching (after two days of troubleshooting with
      @mikhailzrick)
    * For HDMI, block switching while running a game (@mikhailzrick)
    * Fixed auto power led mode (@mikhailzrick)
    * Fixed some file overlay execution bits
    * Added initial RG40xx and SP overlay

--- board/batocera/allwinner/a133/trimui-smart-pro/fsoverlay/usr/bin/poweroff.sh
+++ board/batocera/allwinner/a133/trimui-smart-pro/fsoverlay/usr/bin/poweroff.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Save ALSA mixer state
-# alsactl store 0 -f /userdata/system/.asound.state
-
-# Try to stop all processes cleanly
-/etc/init.d/rcK
 sync
 
 # Start a background process that waits for a timeout duration before forcing shutdown
--- board/batocera/allwinner/h700/fsoverlay/usr/bin/hdmi_audio_switch.sh
+++ board/batocera/allwinner/h700/fsoverlay/usr/bin/hdmi_audio_switch.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+# Path to the HDMI state file
+hdmi_state_file="/sys/devices/platform/soc/6000000.hdmi/extcon/hdmi/state"
+
+check_ingame() {
+    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:1234/runningGame")
+    if [ $? -ne 0 ]; then
+        return 0
+    fi
+    if [ "$HTTP_STATUS" -eq 201 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+if check_ingame; then
+    exit 1
+fi
 
 # Check if debugfs is mounted
 if grep -qs 'debugfs' /proc/mounts; then
@@ -14,48 +33,41 @@ else
     fi
 fi
 
-# Path to the HDMI state file
-hdmi_state_file="/sys/devices/platform/soc/6000000.hdmi/extcon/hdmi/state"
+kill_es() {
+	killall emulationstation 2>/dev/null
+	killall touchegg 2>/dev/null
+	if [ $? -eq 0 ]; then
+		sleep 20 &
+		watchdog=$!
+		while ! [ -z $(pidof emulationstation) ]; do
+			sleep 0.25
+			$(kill -0 $watchdog) || exit
+		done
+		kill -9 $watchdog
+	fi
+}
 
-# Read the state
 state=$(cat "$hdmi_state_file")
-
-display="/sys/kernel/debug/dispdbg"
 
 echo "$(date) - HDMI event triggered - ${state}" >> /var/log/hdmi-event.log
 
-es_state=$(batocera-es-swissknife --espid)
+if [ "$state" = "HDMI=1" ]; then
+    sleep 1
+    batocera-audio set alsa_output._sys_devices_platform_soc_soc_03000000_ahub1_mach_sound_card2.stereo-fallback
+    batocera-resolution setOutput
+    echo "HDMI CONNECTED!" >> /var/log/hdmi-event.log
+    kill_es
 
-if [ "$state" == "HDMI=1" ]; then
-#	batocera-audio set alsa_output.platform-soc_03000000_ahub1_mach.unknown
-	batocera-audio set alsa_output._sys_devices_platform_soc_soc_03000000_ahub1_mach_sound_card2.stereo-fallback
-	
-	echo disp0 > $display/name
-	echo switch1 > $display/command
-	echo 4 10 0 0 0x4 0x101 0 0 0 8 > $display/param
-	echo 1 > $display/start
-
-	if [ "$es_state" != 0 ]; then
-		batocera-es-swissknife --restart
-	fi
-
-	fbset -g 1280 720 1280 1440 32 -t 13426 192 56 22 1 136 3
-	echo "CONNECTED!"
 else
-#	batocera-audio set alsa_output.platform-soc_03000000_codec_mach.unknown
-	batocera-audio set alsa_output._sys_devices_platform_soc_soc_03000000_codec_mach_sound_card0.stereo-fallback
-
-	echo disp0 > $display/name
-        echo switch > $display/command
-        echo 1 0 > $display/param
-        echo 1 > $display/start
-
-        if [ "$es_state" != 0 ]; then
-		batocera-es-swissknife --restart
+    if check_ingame; then
+	    exit 1
 	fi
-
-	fbset -g 640 480 640 960 32
-
-	echo "HDMI DISCONNECTED!"
+	
+	sleep 1
+	batocera-audio set alsa_output._sys_devices_platform_soc_soc_03000000_codec_mach_sound_card0.stereo-fallback
+    batocera-resolution setOutput
+    echo "HDMI DISCONNECTED!" >> /var/log/hdmi-event.log
+    kill_es
 fi
 
+exit 0
